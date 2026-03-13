1b8fd02e194705b4a14d33b2a5bf4efc238cb041
jananisenthilram <jananis920822243021@nprcolleges.org>
2026-03-13 21:15:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,50 @@ images.forEach(img => {
 closeBtn.addEventListener("click", function(){
     popup.style.display = "none";
 });
+let slides = document.querySelectorAll(".slide");
+let texts = document.querySelectorAll(".slide_text");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+slides.forEach((slide)=>{
+slide.classList.remove("active");
+});
+
+texts.forEach((text)=>{
+text.classList.remove("active");
+});
+
+slides[index].classList.add("active");
+texts[index].classList.add("active");
+
+}
+
+function nextSlide(){
+currentSlide++;
+
+if(currentSlide >= slides.length){
+currentSlide = 0;
+}
+
+showSlide(currentSlide);
+}
+
+function prevSlide(){
+currentSlide--;
+
+if(currentSlide < 0){
+currentSlide = slides.length - 1;
+}
+
+showSlide(currentSlide);
+}
+
+document.querySelector(".next").addEventListener("click", nextSlide);
+document.querySelector(".prev").addEventListener("click", prevSlide);
+
+
+/* AUTO SLIDE */
+
+setInterval(nextSlide, 4000);
